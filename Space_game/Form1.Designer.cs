581aaf8bde2551bb72eb2b8c58c8c8bf7a3a33c8
KubaaKs <jakubksiazka@wp.pl>
2020-05-20 21:35:09 +0200

    Implementacja obsługi klawiszy do poruszania, wyswietlenia porazki lub wygranej

--- Space_game/Form1.Designer.cs
+++ Space_game/Form1.Designer.cs
@@ -50,11 +50,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 709);
+            this.label1.Location = new System.Drawing.Point(22, 669);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.Size = new System.Drawing.Size(28, 50);
             this.label1.TabIndex = 0;
-            this.label1.Text = "HP:";
+            this.label1.Text = "Zdrowie:";
             // 
             // label2
             // 
@@ -66,7 +66,6 @@
             this.label2.Size = new System.Drawing.Size(98, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "PUNKTY: 0";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Gracz
             // 
@@ -98,6 +97,18 @@
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.GraczRuch);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Wystrzel);
+            // 
+            // Obserwator
+            // 
+            this.Obserwator.Tick += new System.EventHandler(this.Observe);
+            // 
             // Space_invaders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -111,6 +122,8 @@
             this.Controls.Add(this.label1);
             this.Name = "Space_invaders";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KlawiszPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KlawiszZwolniony);
             ((System.ComponentModel.ISupportInitialize)(this.Gracz)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- Space_game/Form1.cs
+++ Space_game/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,11 +17,169 @@ namespace Space_game
         public Space_invaders()
         {
             InitializeComponent();
+            new Przeciwnik().CreateSprites(this);
+            ///WstawPrzeciwnika
+        }
+        List<PictureBox> obcy = new List<PictureBox>();
+        List<PictureBox> UsunObcy = new List<PictureBox>();
+
+        const int x = 360;
+        const int y = 650;
+        const int limit = 730;
+
+        int predkosc = -1;
+        int lewo =  -1;
+        int gora = 0;
+        int zycia = 0;
+        int pkt = 0;
+
+        bool gra = true;
+        bool ruchLewo;
+        bool ruchPrawo;
+        bool strzal;
+
+        private void KlawiszPress(object sender, KeyEventArgs e) /// obsługa poruszania sie
+        {
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                ruchLewo = true;
+            }
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                ruchPrawo = true;
+            }
+            else if (e.KeyCode == Keys.Space && gra && !strzal)
+            {
+                Pocisk();
+                strzal = true;
+
+            }
+
         }
 
-        private void label2_Click(object sender, EventArgs e)
+
+
+        private void KlawiszZwolniony(object sender, KeyEventArgs e)  ///Zatrzymywanie ruchu gracza
+        {
+            if(e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                ruchLewo = false;
+            }
+            else if(e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                ruchPrawo = false;
+            }
+            else if(e.KeyCode == Keys.Space)
+            {
+                strzal = false;
+            }
+        }
+
+
+
+
+
+        private void Pocisk()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Location = new Point(Gracz.Location.X + Gracz.Width / +2, Gracz.Location.Y - 20);
+            bullet.Size = new Size(5, 20);
+            bullet.BackgroundImage = Properties.Resources.bullet;
+            bullet.BackgroundImageLayout = ImageLayout.Stretch;
+            bullet.Name = "Pocisk";
+            this.Controls.Add(bullet);
+
+        }
+
+        private void GraczRuch(object sender, EventArgs e)
+        {
+            if(ruchLewo && Gracz.Location.X >=0)
+            {
+                Gracz.Left--;
+            }
+            else if(ruchPrawo && Gracz.Location.X <= limit)
+            {
+                Gracz.Left++;
+            }
+        }
+
+        private void Wystrzel(object sender, EventArgs e)
+        {
+            foreach(Control c in this.Controls)
+            {
+                if (c is PictureBox && c.Name == "Pocisk")
+                {
+                    PictureBox bullet = (PictureBox)c;
+                    bullet.Top -= 5;
+
+                    if(bullet.Location.Y <=0)
+                    {
+                        this.Controls.Remove(bullet);
+                    }
+
+                    foreach(Control ct in this.Controls)
+                    {
+                        if(ct is PictureBox && ct.Name == "Laser")
+                        {
+                            /// trzeba dokonczyc
+                        }
+                    }
+                }
+                
+            }
+
+        }
+
+        
+
+        private void Wygrales() /// komunikat z informacja o wygranej
+        {
+            gra = false;
+
+            foreach(Control c in this.Controls)
+            {
+                if(c is Label && c.Name == "Koniec")
+                {
+                    Label lbl = (Label)c;
+                    lbl.Text = "Wygrales !" + "\n" + "Punkty: " + pkt.ToString();
+                }
+                else
+                {
+                    c.Visible = false;
+                }
+            }
+        }
+
+        private void Przegrales() /// zatrzymanie timerow oraz wyswietlenie komunikatu o porazce
+        {
+            timer1.Stop(); timer2.Stop(); timer3.Stop(); timer4.Stop(); timer5.Stop(); Obserwator.Stop();
+
+            foreach(Control c in this.Controls)
+            {
+                if(c is Label && c.Name == "Koniec")
+                {
+                    Label lbl = (Label)c;
+                    lbl.Text = "Przegrales!";
+                    gra = false;
+                }
+                else
+                {
+                    c.Visible = false;
+                }
+            }
+
+        }
+        private void Punkty(int pkt)
+        {
+            label2.Text = "Punkty: " + pkt.ToString();
+        }
+
+        private void Observe(object sender, EventArgs e)
         {
 
         }
     }
 }
+
+
+       
